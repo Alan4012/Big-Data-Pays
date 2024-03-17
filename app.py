a5b441479b3fe4ae54fc6fb052a27cc0cc7b546b
alan <alan3556@hotmail.fr>
2024-03-17 19:16:28 +0100

    Adding pages

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='templates')
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def dashboard():
+    return render_template('index.html')
 
+@app.route('/recherche-de-pays')
+def researchCountries():
+    return render_template('researchCountries/researchCountries.html')
+
+@app.route('/graphique')
+def graphicsCountries():
+    return render_template('graphics/graphics.html')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
